1dd340e0d3f0a4029a317a732f4009d16bcecf0f
augustoliks <carlos.santos110@fatec.sp.gov.br>
2017-10-22 14:18:12 -0200

    up

--- SimplesmenteEncadeadaCircular/Lista.java
+++ SimplesmenteEncadeadaCircular/Lista.java
@@ -12,18 +12,18 @@ public class Lista {
 	void imprimir() {
 		if (inicio == null) {					//Verifica se existe algum "no" a lista encadeada circular
 			System.out.println("Lista vazia");
-		} 
+		}
 		else {
 			System.out.println(); /* quebra de linha na tela */
 
 			No ultimo = inicio;		///Atribui "No ultimo" com o endereco do "inicio", que servira como um marcador inicial para percorrer a lista até seu final, deixando a referencia de "No inicio" intacta
-			
+
 			do {
 				System.out.print(ultimo.conteudo + " ");	//Imprimi o conteudo da referencia do "No ultimo" no momento
 				ultimo = ultimo.proximo;	//Percorre um "no" na lista encadeada
 			} while (ultimo != inicio);		/*Verifica a referencia de "No ultimo" eh a referencia do primeiro no da lista encadeada
 			 									caso for, significa que a lista encadeada circular completou uma volta completa	*/
-		}		
+		}
 	}
 
 	// METODO INSERIR
@@ -33,10 +33,10 @@ public class Lista {
 		no.conteudo = nro;		//Atribui o conteudo ao "no" criado
 
 		if (inicio == null) {	//Verifica se a variavel global "No inicio", esta sem referencia, caso esteja, indica o "no" criado ha duas linhas acima como o primeiro "no" da lista encadeada
-			inicio = no;		//atribui a referencia do "No inicio" com o endereco do novo "No no" criado 
+			inicio = no;		//atribui a referencia do "No inicio" com o endereco do novo "No no" criado
 			inicio.proximo = no;//Por a lista ser circular, se houver apenas um no existente em sua composicao, o proximo no da lista sera o propio inicio
-		} 
-	
+		}
+
 		else {
 			No ultimo = inicio;					//Atribui "No ultimo" com o endereco do "inicio", que servira como um marcador inicial para percorrer a lista até seu final, deixando a referencia de "No inicio" intacta
 			while (ultimo.proximo != inicio) {	//Verifica se o proximo "no" do "No ultimo" eh diferente do "No inicio"
@@ -53,63 +53,63 @@ public class Lista {
 		no.conteudo = nro;		//Atribui o conteudo ao "no" criado
 
 		if (inicio == null) {	//Verifica se a variavel global "No inicio", esta sem referencia, caso esteja, indica o "no" criado ha duas linhas acima como o primeiro "no" da lista encadeada
-			inicio = no;		//atribui a referencia do "No inicio" com o endereco do novo "No no" criado 
+			inicio = no;		//atribui a referencia do "No inicio" com o endereco do novo "No no" criado
 			inicio.proximo = no;//Por a lista ser circular, se houver apenas um no existente em sua composicao, o proximo no da lista sera o propio inicio
-		} 
-	
+		}
+
 		else {
-			No atual = inicio;		
+			No atual = inicio;
 			No depois = inicio.proximo;
-		
+
 			 do{
-				
+
 				if(nro<inicio.conteudo) {
 					inicio = no;
 					no.proximo=depois;
 					depois.proximo=inicio;
 					return;
 				}
-				 
+
 				if (nro<atual.conteudo) {
 					atual.proximo = no;
 					no.proximo=depois;
 					depois.proximo=no;
 					return;
 				}
-				
+
 				atual=depois;
 				depois = depois.proximo;
-			}while(atual!=inicio);			
+			}while(atual!=inicio);
 		}
 	}*/
-	
+
 	No last() {
 		No ultimo = inicio;
 		do {
 			ultimo = ultimo.proximo;
 		}while(ultimo.proximo != inicio);
-		
+
 		return ultimo;
 	}
 
 	int depht(int conteudo) { // Conta os nos da no ateh encontrar o "no" da busca, uma vez encontrado, ele para contar, se nao parar de contar significa q o "no" da busca nao existe
-		No ultimo = inicio; 
+		No ultimo = inicio;
 		boolean contar = true;
 		int k=0;
 		do {
-		
+
 			if (ultimo.conteudo==conteudo) {	//Quando encontra o "no" da busca, para de contar os nos seguintes
-				contar = false;		
-				
+				contar = false;
+
 			}
 
 			if(contar) {	//Inicia a contagem, ou continua...
 				k++;		//Acrescenta +1 ao contador de nos
 			}
-			
+
 			ultimo=ultimo.proximo;	//Avanca a referencia da lista encadeada
-		} while (ultimo != inicio); 
-				
+		} while (ultimo != inicio);
+
 		return contar ? -1 : k;	//Se eu parei de contar (contar==false), significa que eu encontrei o numero, entao retorna a contagem
 								//Se eu nao parei de contar (contar==true), significa que nao encontrei o "no" da busca
 	}
@@ -123,19 +123,19 @@ public class Lista {
 			if(ultimo.conteudo==conteudo) {	//Verifica se o "conteudo" da referencia do "No ultimo" eh igual ao valor do "conteudo" da busca
 				contar=true;	//caso for, inicia a condicao para que uma contagem ate o final da lista encadeada seja executada
 			}
-			if(contar) {	// verifica se pode iniciar a contagem de nos 
+			if(contar) {	// verifica se pode iniciar a contagem de nos
 				k++;		//Acrescenta +1 ao contador de nos
 			}
 			ultimo = ultimo.proximo;	//Avanca a referencia da lista encadeada
-		
+
 		}while (ultimo.proximo != inicio);	//Percorre a lista ate encontrar o "No ultimo" tiver o a referencia igual ao "No inicio", ou seja, verifica toda a lista circular
-			
+
 		return contar ? k : -1;	//Se eu nem comecei a contar (contar==false), significa que NAO encontrei o "no" da busca, entao retorna a -1 indicando erro
 								//Se eu comecei a contar (contar==true), significa que EU ENCONTREI o "no" da busca,  entao retorna a contagem (k)
 
 	}
-	
-	No buscar(int conteudo) {	
+
+	No buscar(int conteudo) {
 		No ultimo = inicio;
 
 		while (ultimo.proximo != inicio && ultimo.conteudo != conteudo) {
@@ -143,30 +143,30 @@ public class Lista {
 		}
 
 		return ultimo.conteudo == conteudo ? ultimo : null;
-	}	
+	}
 
 	void remover(int nro) {
 		if (inicio != null) {
-			
+
 			No anterior = inicio;
 			No atual = inicio.proximo;
-			
+
 			while (atual != inicio && atual.conteudo != nro) {
 				anterior = atual;
 				atual = atual.proximo;
 			}
-			
+
 			if (atual == anterior) {
 				inicio = null;	/* Remove o único nó da lista */
-			} 
+			}
 			else if (atual == inicio) {
 				inicio = inicio.proximo;	/* remove o 1o nó da lista */
 				anterior.proximo = inicio;
-			} 
+			}
 			else {
-				anterior.proximo = atual.proximo; 	/* remove do meio ou final da sequência */		
+				anterior.proximo = atual.proximo; 	/* remove do meio ou final da sequência */
 			}
 		}
 	}
-	
+
 }
